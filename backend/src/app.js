650302c171be5f649abddce4b76b0c46c864cd22
Bruno De Masi <brunodemasi1@gmail.com>
2020-06-25 21:15:22 -0300

    production ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
-mongoose.connect("mongodb://localhost:27017/pi-platform",{
+mongoose.connect('mongodb://localhost:27017/pi-platform', {
   useUnifiedTopology: true,
-  useNewUrlParser: true
-})
+  useNewUrlParser: true,
+});
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const routes = require('./routes')
+const routes = require('./routes');
+
 app.use('/v1', routes);
 
-module.exports = app
+module.exports = app;
