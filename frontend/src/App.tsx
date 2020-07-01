46be1dcee375197bdf12d4f49c995fecd0e2462e
Bruno De Masi <brunodemasi1@gmail.com>
2020-07-01 00:28:09 -0300

    fix: changed ga location

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import Main from './pages/Main';
 
 import GlobalStyle from './styles/global';
 
+ReactGA.initialize('UA-171406858');
+
 const App: React.FC = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  })
+
   return (
     <>
       <Main />
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-171406858');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
